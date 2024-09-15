c27f711ac8f37fb83327391c59e0e38fd5631d70
Jan Augustyn <jaugustyn@student.agh.edu.pl>
2024-09-15 18:17:05 +0200

    env file, postgres, readme, healtcheck, startup file

---
+++ startup.sh
@@ -0,0 +1,12 @@
+# Custom startup script for Django app on Azure App Service
+
+echo "Running startup.sh:"
+
+echo "[1] Run migrations"
+python manage.py migrate
+
+echo "[2] Collect static files"
+python manage.py collectstatic
+
+echo "[3] Run server"
+gunicorn --bind=0.0.0.0 --timeout 600 core.wsgi
