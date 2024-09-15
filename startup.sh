2ce936d076d28f4d1cbd7b0926d5020107f76e3c
Jan Augustyn <jaugustyn@student.agh.edu.pl>
2024-09-15 22:14:52 +0200

    TASK-50_fix - fixing static files (no css and stuff) (#3)

--- startup.sh
+++ startup.sh
@@ -5,9 +5,8 @@ echo "Running startup.sh:"
 echo "[1] Run migrations"
 python manage.py migrate
 
-# For now we don't need to collect static files
-# echo "[2] Collect static files"
-# python manage.py collectstatic
+echo "[2] Collect static files"
+python manage.py collectstatic
 
 echo "[3] Run server"
 gunicorn --bind=0.0.0.0 --timeout 600 backend.wsgi
