c6475e4c5a00f1a663417c47296a76e3ec5acb34
jaugustyn02 <jaugustyn@student.agh.edu.pl>
2024-11-02 15:12:09 +0100

    #6_2 - get parent receivers

--- parent_panel/urls.py
+++ parent_panel/urls.py
@@ -1,10 +1,10 @@
-from re import I
 from django.urls import path
 from . import views
 from .views import ObtainParentTokenPairView
 from .other_views.child_receivers import ChildReceiversView
 from .other_views.parent_data import ParentDataView
 from .other_views.child_details import get_child_details
+from .other_views.parent_receivers import ParentReceiversView
 
 urlpatterns = [
     path("", ParentDataView.as_view()),
@@ -13,5 +13,6 @@ urlpatterns = [
     path("generate/<int:id>", views.generate_QR_code, name="generate_QR_code"),
     path("child/<int:child_id>", get_child_details, name="get_child_details"),
     path("child/<int:child_id>/receivers", ChildReceiversView.as_view()),
-    path("change-password", views.change_password, name='change-password')
+    path("change-password", views.change_password, name='change-password'),
+    path("receivers", ParentReceiversView.as_view(), name="get_parent_receivers")
 ]
