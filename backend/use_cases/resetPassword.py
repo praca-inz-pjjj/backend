31b9bfbd42825ceb8472373a7282439f65c1b7eb
Jan Augustyn <jaugustyn@student.agh.edu.pl>
2024-11-22 02:55:11 +0100

    Small reset password mail improvement, permissions delete (#40)
    * Small reset password mail improvement
    
    * quick add receiver signature fix
    
    * usuwanie permisji

--- backend/use_cases/resetPassword.py
+++ backend/use_cases/resetPassword.py
@@ -1,16 +1,10 @@
-from email.message import EmailMessage
-import smtplib
-import ssl
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.core.mail import send_mail
-from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from django.urls import reverse
 
 from backbone.models import CustomUser
-from backend.settings import DEFAULT_FROM_MAIL, EMAIL_HOST, EMAIL_HOST_PASSWORD, EMAIL_HOST_USER, EMAIL_PORT, FRONTEND_LINK, SENDER_NAME, WEBSITE
+from backend.settings import FRONTEND_LINK
 from backend.utils.sendmail import sendmail
 
 class PasswordResetRequestView(APIView):
@@ -18,13 +12,27 @@ class PasswordResetRequestView(APIView):
     def post(self, request):
         email = request.data.get("email")
         try:
+            if not email:
+                return Response({"message": "Nie podano adresu email."}, status=status.HTTP_400_BAD_REQUEST)
+            if not CustomUser.objects.filter(email=email).exists():
+                return Response({"message": "Użytkownik o podanym adresie email nie istnieje."}, status=status.HTTP_404_NOT_FOUND)
             user = CustomUser.objects.get(email=email)
             token_generator = PasswordResetTokenGenerator()
             token = token_generator.make_token(user)
             reset_url = f"{FRONTEND_LINK}/reset-password/{user.pk}/{token}/"
             print("reset url:", reset_url)
-            text = f"<p>Oto Twój link do resetu hasła: <a href=\"{reset_url}\">link</a></p>"
-            subject = "Prośba o reset hasła"
+            
+            subject = "Resetowanie hasła"
+            text = f"""
+            <p>Cześć, {user.first_name}!</p>
+            <p>Otrzymaliśmy prośbę o zresetowanie Twojego hasła. Jeśli ta prośba nie pochodzi od Ciebie, zignoruj to powiadomienie. W przeciwnym razie możesz zresetować hasło za pomocą tego linku:</p>
+            <p style="text-align: center;">
+                <a href="{reset_url}" 
+                   style="background-color: #007bff; color: white; padding: 15px 30px; font-size: 16px; text-decoration: none; border-radius: 5px; display: inline-block;">
+                   Resetuj hasło
+                </a>
+            </p>
+            """
             return sendmail(email, subject, text)
         except:
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
