5b65adefb628789cd14e1e179bab762097770cb8
jkr02 <jakubkroczek02@gmail.com>
2024-12-19 15:52:31 +0100

    usuniete nieuzywane importy i printy

--- parent_panel/other_views/receiverPermission.py
+++ parent_panel/other_views/receiverPermission.py
@@ -1,24 +1,13 @@
-from django.utils import timezone
-from email import message
-from http.client import BAD_REQUEST
-import json
 from urllib.request import Request
 
-from django.shortcuts import get_object_or_404
-from backbone.models import CustomUser, Log
+from backbone.models import CustomUser
 from backbone.permisions import IsParent
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from backbone.serializers import CustomUserSerializer, PermittedUserSerializer
-from backbone.types import LogType
+from backbone.serializers import CustomUserSerializer
 from parent_panel.models import PermittedUser, UserChild
-from parent_panel.other_views.commons import EMAIL_IS_ALREADY_TAKEN_MESSAGE, NO_ACCESS_TO_CHILD_RESPONSE_MESSAGE, PHONE_IS_ALREADY_TAKEN_MESSAGE
-from parent_panel.other_views.validators.child_validator import ChildValidator
-from parent_panel.other_views.validators.user_validator import UserValidator
-from parent_panel.serializers import UserChildrenSerializer
-from teacher_panel.models import Child
 
 class ReceiverPermission(APIView):
     permission_classes = (IsParent, )
@@ -42,7 +31,6 @@ class ReceiverPermission(APIView):
             permission = PermittedUser.objects.filter(child_id=child_id, user_id=receiver_id, parent_id=parent.id)
             if len(permission) > 0:
                 return Response(status=status.HTTP_400_BAD_REQUEST)
-            print("elo1")
             try:
                 permittedUser = PermittedUser.objects.create(user_id=receiver_id, child_id=child_id, parent_id=parent.id)
                 permittedUser.save()
