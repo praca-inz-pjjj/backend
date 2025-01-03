d3d6264cbb62187d5ab085a1253a6bba580e120d
pwolan <49585424+pwolan@users.noreply.github.com>
2025-01-03 17:00:10 +0000

    removed prints

--- parent_panel/views.py
+++ parent_panel/views.py
@@ -111,8 +111,6 @@ def create_permission(request, id):
                                             "end_date": request.data['end_date']})
     except:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    print("OK")
-    print(serializer.error_messages)
     if serializer.is_valid():
         permission = serializer.save()
         if request.data['two_factor_verification']:
